3e132b925ccadc25cca29b5b3852e4574879ea88
ʀɪᴏ sʜɪɴ <antmangktifjfjtjfjft@gmail.com>
2025-12-27 12:18:34 +0530

    Create restart.py

---
+++ TG/restart.py
@@ -0,0 +1,24 @@
+# TG/restart.py
+import os
+import sys
+import asyncio
+from pyrogram import filters
+
+from bot import Bot, ADMINS
+
+
+@Bot.on_message(filters.command(["restart", "reboot"]) & filters.user(ADMINS))
+async def restart_cmd(_, msg):
+    m = await msg.reply_text("♻️ Restarting bot...")
+
+    # Give Telegram time to send the message
+    await asyncio.sleep(1.5)
+
+    # Try graceful stop (optional)
+    try:
+        await Bot.stop()
+    except Exception:
+        pass
+
+    # On Render, exiting makes the service start again automatically
+    os._exit(0)
