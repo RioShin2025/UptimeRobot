afa2c013e24b7eb400fa5d3b7750cdf02de1b3c3
ʀɪᴏ sʜɪɴ <antmangktifjfjtjfjft@gmail.com>
2025-12-27 11:10:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bot import Bot
 
+from TG.broadcast import load_broadcast_cmds
+load_broadcast_cmds(Bot, Vars, uts, sync)
 from Tools.db import get_all_urls, remove_expired_urls
 from Tools.requets_handler import runs_checking
 import asyncio
